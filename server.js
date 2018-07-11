5f9f5c09903e4ba489904df8aefc2c2d5956a4b4
ankit <ankitsingh80741@gmail.com>
2018-07-12 02:38:14 +0530

    profile register service added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
+
+// body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
 //DB config
 const db = require('./config/key').mongoURI;
 mongoose.connect(db).then(() => console.log("MONGOdb connected")).catch(err => console.log("error occured", err));
